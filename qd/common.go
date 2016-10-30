9fd9a6ebb06262aea69c132a13b212dedc75041d
Eeo Jun <141bytes@gmail.com>
2016-10-30 15:57:41 +0800

    update

--- qd/common.go
+++ qd/common.go
@@ -8,13 +8,15 @@ func midpoint(a, b float64) float64 {
 }
 
 func BoundFromPoints(xs []*geo.Point) *geo.Bound {
-	min_x := xs[0].X(); min_y := xs[0].Y();
-	max_x := xs[0].X(); max_y := xs[0].Y();
+	min_x := xs[0].X()
+	min_y := xs[0].Y()
+	max_x := xs[0].X()
+	max_y := xs[0].Y()
 	for _, p := range xs[1:] {
-		min_x = math.Min(min_x, p.X());
-		min_y = math.Min(min_y, p.Y());
-		max_x = math.Max(max_x, p.X());
-		max_y = math.Max(max_y, p.Y());
+		min_x = math.Min(min_x, p.X())
+		min_y = math.Min(min_y, p.Y())
+		max_x = math.Max(max_x, p.X())
+		max_y = math.Max(max_y, p.Y())
 	}
 	return geo.NewBound(min_x, max_x, min_y, max_y)
 }
--- qd/main.go
+++ qd/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 	"github.com/paulmach/go.geo"
+	"os"
+	"strings"
 )
+
+func printNode(node *Node) {
+	fmt.Println(strings.Repeat(" ", node.Depth-1), node)
+	for _, child := range node.Children {
+		printNode(child)
+	}
+}
 
 func countNodes(node *Node, count int) int {
 	count++
@@ -47,3 +55,31 @@ func main() {
 		}
 	}
 }
+
+
+/*
+func main() {
+	points := []*geo.Point{
+		&geo.Point{0, 1},
+		&geo.Point{3, 0},
+		&geo.Point{3, 5},
+		&geo.Point{6, 5},
+		&geo.Point{8, 3},
+	}
+	triangles := []*Triangle{
+		NewTriangleFromPoints(points[0], points[1], points[2]),
+		NewTriangleFromPoints(points[1], points[2], points[3]),
+		NewTriangleFromPoints(points[1], points[3], points[4]),
+	}
+	b := BoundFromPoints(points)
+	fmt.Println(b.NorthWest())
+	fmt.Println(b.NorthEast())
+	fmt.Println(b.SouthWest())
+	fmt.Println(b.SouthEast())
+
+	q := NewNode(b, 1)
+	q.AddTriangles(triangles)
+	q.Partition(1, 3)
+	printNode(q)
+}
+*/
